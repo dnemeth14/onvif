7210ad1301c7d52b2ffe78a6e727b1653c4be7c0
dnemeth14 <dnemeth1714@gmail.com>
2021-09-09 11:33:19 -0700

    Added print doc.Root() to device.go

--- Device.go
+++ Device.go
@@ -104,7 +104,7 @@ func (dev *Device) GetDeviceInfoAsStr() string {
 }
 
 func (dev *Device) GetDeviceParamsAsStr() string {
-	return ("Xaddr: " + dev.params.Xaddr + ", Username: " + dev.params.Username + ", Password: " + dev.params.Password)	
+	return ("Xaddr: " + dev.params.Xaddr + ", Username: " + dev.params.Username + ", Password: " + dev.params.Password)
 }
 
 func (dev *Device) GetEndPointsAsStr() string {
@@ -138,19 +138,25 @@ func GetAvailableDevicesAtSpecificEthernetInterface(interfaceName string, debugO
 	for _, j := range devices {
 		doc := etree.NewDocument()
 		if err := doc.ReadFromString(j); err != nil {
-			if debugOn {fmt.Errorf("%s", err.Error())}
+			if debugOn {
+				fmt.Errorf("%s", err.Error())
+			}
 			return nil
 		}
 
 		endpoints := doc.Root().FindElements("./Body/ProbeMatches/ProbeMatch/XAddrs")
 		for _, xaddr := range endpoints {
 			xaddr := strings.Split(strings.Split(xaddr.Text(), " ")[0], "/")[2]
-			if debugOn {fmt.Println(xaddr)}
+			if debugOn {
+				fmt.Println(xaddr)
+			}
 			c := 0
 
 			for c = 0; c < len(nvtDevices); c++ {
 				if nvtDevices[c].params.Xaddr == xaddr {
-					if debugOn {fmt.Println(nvtDevices[c].params.Xaddr, "==", xaddr)}
+					if debugOn {
+						fmt.Println(nvtDevices[c].params.Xaddr, "==", xaddr)
+					}
 					break
 				}
 			}
@@ -159,6 +165,9 @@ func GetAvailableDevicesAtSpecificEthernetInterface(interfaceName string, debugO
 				continue
 			}
 
+			fmt.Println("|||||||||||||||||||||||||||||")
+			fmt.Println(doc.Root())
+			fmt.Println("|||||||||||||||||||||||||||||")
 			dev, err := NewDevice(DeviceParams{Xaddr: strings.Split(xaddr, " ")[0]})
 
 			if err != nil {
